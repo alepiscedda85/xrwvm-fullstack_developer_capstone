787bccc623f92a3d5452cb00ea54292ba91bec1b
Alessio <alepiscedda85@gmail.com>
2026-04-13 11:23:28 -0400

    initial commit

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,19 @@
-import LoginPanel from "./components/Login/Login"
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Dealers from "./components/Dealers/Dealers";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/" element={<Dealers />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;

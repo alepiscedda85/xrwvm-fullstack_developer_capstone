c9b088328646fba53293e1b3fa594a7cfc7e426f
aLESSIO <alepiscedda85@gmail.com>
2026-04-15 10:49:28 -0400

    Lab: Crea una Vista Django per Pubblicare una Recensione di un Rivenditore

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from .restapis import get_request, analyze_review_sentiments, post_review
 from . import views
 
 app_name = 'djangoapp'
@@ -9,4 +10,10 @@ urlpatterns = [
     path('register', views.registration, name='register'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),     path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

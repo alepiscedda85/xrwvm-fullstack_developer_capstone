4c90f700de6445cefe1af9f1a10b7b22dae00800
aLESSIO <alepiscedda85@gmail.com>
2026-04-15 11:23:10 -0400

    Lab: Passaggi per Registrare i Modelli CarMake e CarModel con il Sito di Amministrazione

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -30,8 +30,8 @@ def get_request(endpoint, **kwargs):
 
 
 def analyze_review_sentiments(text):
-request_url = sentiment_analyzer_url+"analyze/"+text
-try:
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()

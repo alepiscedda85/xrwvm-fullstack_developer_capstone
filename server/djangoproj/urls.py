9c8415cf43200005689f7168b2da06328ca55f83
Alessio <alepiscedda85@gmail.com>
2026-04-18 16:58:33 -0400

    Crea una Pagina di Dettagli o Recensioni del Rivenditore-COMPLETATO

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -7,17 +7,20 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # 🔥 API BACKEND DJANGO
+    # API Django
     path('djangoapp/', include('djangoapp.urls')),
 
-    # 🔥 REACT APP (serve sempre index.html)
-    path('', TemplateView.as_view(template_name="index.html")),
+    # Pagine HTML classiche
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+
+    # React app
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-
-    # 🔥 PAGINA DEALER (richiesta dal lab)
     path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Dealers from "./components/Dealers/Dealers";
 import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Dealers />} />
       <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
